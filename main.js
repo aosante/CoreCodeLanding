bcfe08ce68bb2da9de27640cade0186f06678e12
Andrés Osante <aosantea@ucenfote.ac.cr>
2019-02-22 19:53:36 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  let scrollLink = $('.scroll');
+
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({ scrollTop: $(this.hash).offset().top }, 900);
+  });
+});
